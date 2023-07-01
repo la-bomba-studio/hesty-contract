8e80bd702135e6d58f5902d41e5d3cff8dd1b35e
neel-enstead <neel@enstead.com>
2023-07-02 00:23:39 +0530

    Added Contract Deployment Scripts

---
+++ scripts/deploy-property-token.js
@@ -0,0 +1,50 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  let PropertyToken;
+  let propertyToken;
+  try {
+    await hre.run("compile");
+    PropertyToken = await hre.ethers.getContractFactory("PropertyTokenFactory");
+    propertyToken = await upgrades.deployProxy(
+      PropertyToken,
+      [process.env.ADMIN_WALLET_ADDRESS],
+      { kind: "uups" }
+    );
+    await propertyToken.deployed();
+  } catch (err) {
+    console.log("Contract deployment failed", err);
+  }
+
+  const waitForDeployment = (seconds) => {
+    console.log(
+      `\n\x1b[1m\x1b[33m${"[pending]"}\x1b[0m Preparing PropertyToken smart contract deployment. Just a moment... \n`
+    );
+    setTimeout(() => {
+      console.log(
+        `\x1b[1m\x1b[32m${"[success]"}\x1b[0m PropertyToken smart contract deployed successfully to: \x1b[4mhttps://mumbai.polygonscan.com/address/${
+          propertyToken.address
+        }#code\x1b[0m \n`
+      );
+    }, seconds);
+  };
+  waitForDeployment(3000);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
